770c7614fa3a284f5acae2fa00e95c934d7a18cc
arkar20 <arkar20011@gmail.com>
2024-08-24 19:35:22 -0700

    local strategy and jwt strategy

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -6,19 +6,30 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
+  Req,
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateAuthDto } from './dto/create-auth.dto';
 import { UpdateAuthDto } from './dto/update-auth.dto';
 import { LoginAuthDto } from './dto/login-auth.dto';
+import { LocalGuard } from './guards/local.guard';
+import { JwtGuard } from './guards/jwt.guard';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('/login')
+  @UseGuards(LocalGuard)
   login(@Body() loginDto: LoginAuthDto) {
     return this.authService.validateUser(loginDto);
+  }
+
+  @Get('/profile')
+  @UseGuards(JwtGuard)
+  profile(@Req() req) {
+    return this.authService.findOne(req.user.id);
   }
 
   @Post()
